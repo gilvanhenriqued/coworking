b1235adc9450ca63be3a8094e46e0b28867ae2f7
IgorOliveira8 <igor_oliveira97@hotmail.com>
2017-06-20 10:34:44 -0300

    Teste de autenticação

--- test/teste.js
+++ test/teste.js
@@ -6,7 +6,7 @@ let server = require('../index');
 let should = chai.should();
 
 chai.use(chaiHttp);
-
+// -------------------------------------------- //
   describe('Users', ()=>{
     describe('/GET user', ()=>{
       it('should save without error', ()=>{
@@ -18,3 +18,49 @@ chai.use(chaiHttp);
       })
     })
   })
+  // ---------------------------------------------------------------------- //
+  describe('/POST user', () => {
+        it('it should authenticate without errors', (done) => {
+          let user = {
+              username: "user1",
+              password: "user1",
+              first_name: "User",
+              last_name: "One",
+              email: "userone@gmail.com"
+          }
+          let successfulAuth = {
+              username: "user1",
+              password: "user1",
+          }
+          let unsuccessfulAuth = {
+              username: "user1",
+              password: "user12",
+          }
+          chai.request(server)
+              .post('/api/users')
+              .send(user)
+              .end((err, res) => {
+                chai.request(server)
+                    .post('/api/autentication_routes')
+                    .send(unsuccessfulAuth)
+                    .end((err, res) => {
+                        res.should.have.status(200);
+                        res.body.should.be.a('object');
+                        res.body.should.have.property('success').eql(false);
+                        res.body.should.have.property('message');
+                    });
+                chai.request(server)
+                    .post('/api/autentication_routes')
+                    .send(successfulAuth)
+                    .end((err, res) => {
+                        res.should.have.status(200);
+                        res.body.should.be.a('object');
+                        res.body.should.have.property('success').eql(true);
+                        res.body.should.have.property('token');
+                        res.body.should.have.property('user');
+                    });
+                done();
+              });
+        });
+      });
+  //-------------------------------------------------------------------------------------------//

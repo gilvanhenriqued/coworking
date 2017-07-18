2095dcd44d31622f7b41665a33109ee58a2b7db4
GilvanHenrique <gilvanh.15@gmail.com>
2017-07-18 10:36:13 -0300

    Mensagem de erro no login funcionando

--- public/app/components/authentications/authenticationCtrl.js
+++ public/app/components/authentications/authenticationCtrl.js
@@ -1,22 +1,22 @@
 angular.module('coworking').controller('AuthenticationCtrl', AuthenticationCtrl);
 
-function AuthenticationCtrl($scope, $rootScape, $routeParams, $location, authSvc) {
+function AuthenticationCtrl($scope, $rootScope, $routeParams, $location, authSvc) {
 var self = this;
   self.email = "";
   self.senha = "";
   self.erro = false;
 
-self.autenticar = function(req, res){
-  authSvc.login(self.email, self.senha).then(
-    function(res){
-      if(res.data.success){
-        console.log(res.data);
-        $scope.data = res.data;
-        $rootScape.$broadcast('erro', {message: "Erro! Email ou senha incorretos."})
-      }else{
-        self.erro = true;
-      }
-    }
-  )}
-
+  self.autenticar = function(){
+    authSvc.login(self.email, self.senha).then(function(res){
+        if(res.data.success){
+          console.log(res.data);
+          console.log('Logado com sucesso!');
+          $location.path('/home');
+        }else{
+          console.log(res.data);
+          $scope.data = res.data;
+          $rootScope.$broadcast('evento', {alerta: "erro", message: "Erro! Email ou senha incorretos."})
+        }
+      })
+  }
 }
